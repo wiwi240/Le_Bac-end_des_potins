f2d0a2cbe5e7f37c777aca115937b206e94bdfda
wiwi240 <william.mahi14@gmail.com>
2026-02-19 23:40:50 +0100

    add background header

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,10 @@ class StaticPagesController < ApplicationController
   def home
     # Sélection des 5 potins les plus aimés
     @gossips = Gossip.left_joins(:likes)
-                     .group(:id)
-                     .order('COUNT(likes.id) DESC')
-                     .limit(5)
-  end
+    .group(:id)
+    .order('COUNT(likes.id) DESC')
+    .limit(5)
+end
 
   def team
   end
